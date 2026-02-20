3bb85c5365bbcff265a4f7edb6f9db635dfcd552
Jonas Dossmann <dossma@mailbox.org>
2026-02-20 16:27:31 +0000

    Cleaned mixed language into plain English, removed some remarks and commented commands

--- Telegram-download-voice-messages.py
+++ Telegram-download-voice-messages.py
@@ -1,5 +1,5 @@
 from telethon import TelegramClient, events, sync
-from telethon.tl.types import InputMessagesFilterRoundVoice  # Filter (z.B. Sprachnachricht, Photos, usw. siehe https://tl.telethon.dev/constructors/index.html)
+from telethon.tl.types import InputMessagesFilterRoundVoice
 import os.path
 #from os.path import exists
 import logging
@@ -9,107 +9,105 @@ Notice:
 This program uses the Telegram API and is part of the Telegram ecosystem.
 '''
 
-# Parameter:
+# Input parameter:
 folder = r"f:\path\to\your\targetfolder"  # The folder where your files and the ID repository file are to be saved.
-channel = "channelname"  # Type here in the channel's name. The full link  https://t.me/channelname probably works too.
+channel = "channelname"  # Type here in the channel's name. The full link  https://t.me/channelname should work too.
 limit = 100  # value which indicates how many files you want to download, set it to <None> for no limit
 api_id = 1234567  # get your api_id from https://core.telegram.org/api/obtaining_api_id#obtaining-api-id
-api_hash = 'tpyeHereYourAPI-hash'  # get your api_hash from https://core.telegram.org/api/obtaining_api_id#obtaining-api-id
+api_hash = 'pasteHereYourAPIhash'  # get your api_hash from https://core.telegram.org/api/obtaining_api_id#obtaining-api-id
+# --- You're done now. Start the program. --- 
 
-os.chdir(folder)  # Arbeitsverzeichnis zum Zielordner ändern
+os.chdir(folder)  # Change working directory to target directory
 logging.basicConfig(filename='log.log', filemode='w', level=logging.DEBUG)
 
 client = TelegramClient('session_name', api_id, api_hash)
-print("Starte Client")
+print("Starting Client")
 client.start()
-logging.info("Client gestartet")
+logging.info("Client startet")
 
-print("Einträge einholen")
+print("Collect entries")
 msgs = client.get_messages(entity=channel, limit=limit, filter=InputMessagesFilterRoundVoice)
-print(len(msgs), "Einträge vorhanden")
+print(len(msgs), "entries collected")
 
-errlist = []  # Nicht heruntergeladene message-Namen
-skipped = []  # Nicht heruntergeladene message-Namen
-counter = 0  # Zähler wieviele Nachrichten übrig sind
-counter_unknown = 0  # Zähler wieviele Nachrichten übrig sind
+errlist = []  # Not downloaded message-names due to error
+skipped = []  # Not downloaded message-names due to already downloaded
+counter = 0  # Counter how many messages are left
+counter_unknown = 0  # Counter for messages left which have no name attribute
 
 # Establishing list of voice message IDs to prevent downloading duplicates
-id_rep_file = open('id_list.txt', 'a')  # Liste mit gespeicherten IDs, a+ für erstellen bei Bedarf, lesen + schreiben
+id_rep_file = open('id_list.txt', 'a')  # List with saved IDs, a+ for establishing as required, read + write
 id_rep = id_rep_file.read().splitlines()  # id repository
-id_container = []  # Liste mit IDs, die nun von Telegram abgefragt werden
+id_container = []  # List with IDs, which are being requested from Telegram
 
-for msg in msgs:  # IDs sammeln
+for msg in msgs:  # Collect IDs
     id_container.append(msg.file.id)
-    # id_list_lines.append(msg.file.id)  # zusammengefügte Liste mit vorhandenen IDs und jenen von Telegram
 
-# missingidlist = [item for item in id_list_lines if item not in id_container]  # Alternativer Ausdruck
-# missingidlist = set(id_list_lines)  #  Alle IDs löschen, die nun doppelt vorhanden sind: Jene Dateien fehlen und sind herunterzuladen
-missingidlist = list(set(id_container).difference(id_rep))  # Ausfiltern von doppelter Einträge aus den zwei Listen. Resultat sind die herunterzuladenen Dateien.
+missingidlist = list(set(id_container).difference(id_rep))  # Filtering of duplicate entries from the two lists. Result is the files to be downloaded.
 
 for msg in msgs:
     counter += 1
 
     file_id = msg.file.id
-    titel = msg.text  # oder möglich: msg.message
-    if any(file_id in s for s in missingidlist):  # Datei ist noch nicht vorhanden, also herunterladen
+    title = msg.text  # or possible: msg.message
+    if any(file_id in s for s in missingidlist):  # File id is not in list, therefore download it
 
-        endung = msg.file.ext
-        creatime = msg.date.date().isoformat()  # Datumsformat: yyyy-mm-dd
-        views = msg.views  # views speichern
-        views = round(views / 100) * 100  # Auf hunderter runden
-        views = str(views)  # in Text konvertieren
+        ext = msg.file.ext
+        creatime = msg.date.date().isoformat()  # Date format: yyyy-mm-dd
+        views = msg.views  # save views
+        views = round(views / 100) * 100  # Round to hundreds
+        views = str(views)  # convert into string
 
-        if titel:  # oder möglich: msg.message
-            filename = creatime + " " + titel + " " + views + "views" + endung  # Name + Dateiendung
+        if title:  # oder möglich: msg.message
+            filename = creatime + " " + title + " " + views + "views" + ext  # Name + file extension
         else:
-            filename = creatime + " unknown" + str(counter_unknown) + " " + views + "views" + endung
+            filename = creatime + " unknown" + str(counter_unknown) + " " + views + "views" + ext
             counter_unknown += 1
 
-        # Unerlaubte Dateizeichen ersetzen
-        filename = filename.replace("\n", " - ")  # "\n" ersetzen mit Bindestrich
-        filename = filename.replace("\\", "-")  # "n" ersetzen mit Bindestrich
-        filename = filename.replace("/", "-")   # "/" ersetzen mit Bindestrich
-        filename = filename.replace("?", "-")   # "/" ersetzen mit Bindestrich
-        filename = filename.replace("\"", "!")   # "/" ersetzen mit Bindestrich
-        filename = filename.replace(r"*", " ! ")   # "/" ersetzen mit Bindestrich
-        filename = filename.replace(r"<", " ! ")   # "/" ersetzen mit Bindestrich
-        filename = filename.replace(r">", " ! ")   # "/" ersetzen mit Bindestrich
-        filename = filename.replace(r":", " - ")   # "/" ersetzen mit Bindestrich
+        # Replace characters which are not supported in Windows
+        filename = filename.replace("\n", " - ")
+        filename = filename.replace("\\", "-")
+        filename = filename.replace("/", "-")
+        filename = filename.replace("?", "-")
+        filename = filename.replace("\"", "!")
+        filename = filename.replace(r"*", " ! ")
+        filename = filename.replace(r"<", " ! ")
+        filename = filename.replace(r">", " ! ")
+        filename = filename.replace(r":", " - ")
 
-        try:  # Datei ist noch nicht im Verzeichnis. Versuchen, sie herunterzuladen
-            print("Herunterladen von Nr.", counter, "von", len(msgs) - counter, "\n", filename, "File ID:", msg.file.id)
+        try:  # File is not yet in directory. Attempt to download it
+            print("Downloading of No.", counter, "of", len(msgs) - counter, "\n", filename, "File ID:", msg.file.id)
             msg.download_media(filename)
-            print("msg:", filename, "runtergeladen")
-            # print(len(msgs) - counter, " übrig", "\n")
-            # id_list.append(msg.id)  # Erfolgreiche Message-ID in Liste speichern
+            print("msg:", filename, "downloaded")
+            # print(len(msgs) - counter, " left", "\n")
         except Exception as inst:
             print(type(inst))  # the exception instance
             print(inst.args)  # arguments stored in .args
             print(inst)  # __str__ allows args to be printed directly,
-            # print ("msg:\n", filename, "\nkonnte nicht heruntergeladen werden")
-            logging.info("msg:", filename, "ID:", file_id, "konnte nicht heruntergeladen werden\n")
+            # print ("msg:\n", filename, "\ncould not be downloaded")
+            logging.info("msg:", filename, "ID:", file_id, "could not be downloaded\n")
             errlist.append(filename)
 
-        id_rep_file.write(file_id+"\n")  # heruntergeladene ID einpflegen
+        id_rep_file.write(file_id+"\n")  # add downloaded file ID
 
-    else:  # Datei ist vorhanden, also nicht herunterladen:
-        logging.debug("Überspringe Titel: %s, ID: %s" % (titel, file_id))
+    else:  # File is there, therefore do not download:
+        logging.debug("Skipping title: %s, ID: %s" % (title, file_id))
 
-        print(len(msgs) - counter, " übrig", "\n")
-        skipped.append(titel)  # Liste füllen übersprungener Dateien
-        skipped.append(file_id)  # Liste füllen übersprungener Dateien
+        print(len(msgs) - counter, " left", "\n")
+        skipped.append(title)  # Fill list of skipped files
+        skipped.append(file_id)  # Fill list of skipped files
 
 id_rep_file.close()
 client.disconnect()
 
-errlist_string = "\n".join(errlist)  # Konvertierung in einen String für Logger
-skipped_string = "\n".join(skipped)  # Konvertierung in einen String für Logger
+errlist_string = "\n".join(errlist)  # Convert in string for logger
+skipped_string = "\n".join(skipped)  # Convert in string for logger
 
-# print("Einträge runterladen abgeschlossen\n")
-logging.info("\nEinträge runterladen abgeschlossen")
-# print("Konnten nicht heruntergeladen werden:", errlist, "\n")
-logging.info("Konnten nicht heruntergeladen werden:\n + %s" % errlist_string)
-# print("Anzahl übersprungener Dateien:", len(skipped), "\n", "Dateien:", skipped)
-logging.info("Übersprungene Dateien:\n %s" % skipped_string)
+# print("Downloading entries finished\n")
+logging.info("\nDownloading entries finished")
+# print("Could not be downloaded:", errlist, "\n")
+logging.info("Could not be downloaded:\n + %s" % errlist_string)
+# print("Number of skipped files:", len(skipped), "\n", "Files:", skipped)
+logging.info("Skipped files:\n %s" % skipped_string)
 
-# os.system("shutdown -s -t 10")  # Computer herunterfahren, letzte Zahl ist Timer in Sekunden
+# os.system("shutdown -s -t 10")  # Shutdown computer, last number is timer in seconds
+
